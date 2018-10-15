09783753b9f1895c5b8a154b79f90644bf9e6e7e
rahulbh123 <44047046+rahulbh123@users.noreply.github.com>
2018-10-15 10:09:47 +0530

    sdg

--- rectangle.py
+++ rectangle.py
@@ -1,6 +1,7 @@
+nxnfnfgnn
 a=int(input("enter the length number:- "))
-b=int(input("enter the breath number:- "))
+b=int(input("enterfgjxdfj the breath number:- "))
 def rectangle(a,b):
-    return(a*b)
+    return(a*b)mfmxcj xfm
 print("the area of rectangle is",rectangle(a,b))    
     
